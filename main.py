1d150f241cae76a5f63c437158b8f8e623d69c17
maximpopov11 <icecreamguy0@gmail.com>
2024-03-13 09:55:00 -0500

    Implement map province parsing
    * Determines province adjacencies
    * Determines province owners
    * Crashes on given asset file due to province 22 being incorrect
    * Centers not yet parsed

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+import numpy as np
+from PIL import Image
+from scipy import ndimage
+from skimage.segmentation import expand_labels, find_boundaries
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+MAP_IMAGE = "assets/provinces.png"
 
+BORDER_COLOR = [0, 0, 0, 255]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+COLOR_PROVINCE_TYPE_MAP = {
+    (96, 96, 96, 255): "impassable",
+    (197, 223, 234, 255): "ocean",
+    (196, 143, 133, 255): "brown",
+    (255, 56, 50, 255): "red",
+    (255, 203, 91, 255): "yellow",
+}
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    provinces = np.asarray(Image.open(MAP_IMAGE).convert('RGBA'))
+    province_ids, num_provinces = ndimage.label((provinces != BORDER_COLOR).any(-1), structure=np.ones((3, 3)))
+    province_ids_expanded = expand_labels(province_ids, distance=6)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    adjacencies = {}
+    for province_id in range(1, num_provinces + 1):
+        boundaries = find_boundaries(province_ids_expanded == province_id, mode='outer')
+        adjacencies[province_id] = np.setdiff1d(np.unique(province_ids_expanded * boundaries), [0])
+
+    province_owners = {}
+    for province_id in range(1, num_provinces+1):
+        colors = np.unique(provinces[province_ids == province_id], axis=0)
+        # assert len(colors) == 1, f"Province #{province_id} is not one solid color: {colors}."
+        # assert tuple(colors[0]) in COLOR_PROVINCE_TYPE_MAP, \
+        #     f"{tuple(colors[0])} is not in the color to province type dictionary (province #{province_id})."
+        province_owners[province_id] = COLOR_PROVINCE_TYPE_MAP[tuple(colors[0])]
