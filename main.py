028bdd649df7bcea84c5ed221a757abc47cf61f3
maximpopov11 <icecreamguy0@gmail.com>
2024-03-26 10:27:14 -0500

    Refactor all

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 from scipy import ndimage
@@ -17,29 +16,40 @@ COLOR_PROVINCE_TYPE_MAP = {
 }
 
 
-# Debugging tool to visualize which province corresponds to which id
-def draw_provinces_by_id(labels):
-    _, ax = plt.subplots()
-    ax.imshow(labels, cmap=plt.cm.gray, vmin=0, vmax=1)
-    plt.show()
-
-
-if __name__ == '__main__':
+def read_map_data():
     provinces = np.asarray(Image.open(MAP_IMAGE).convert('RGBA'))
-    province_ids, num_provinces = ndimage.label((provinces != BORDER_COLOR).any(-1), structure=np.ones((3, 3)))
-    province_ids_expanded = expand_labels(province_ids, distance=6)
+    province_id_map, num_provinces = ndimage.label((provinces != BORDER_COLOR).any(-1), structure=np.ones((3, 3)))
+    province_id_map_expanded = expand_labels(province_id_map, distance=6)
 
+    adjacencies = get_adjacencies(province_id_map_expanded, num_provinces)
+    province_owners = get_province_owners(provinces, province_id_map, num_provinces)
+
+
+def get_adjacencies(province_id_map_expanded, num_provinces):
     adjacencies = {}
-    for province_id in range(1, num_provinces + 1):
-        boundaries = find_boundaries(province_ids_expanded == province_id, mode='outer')
-        adjacencies[province_id] = np.setdiff1d(np.unique(province_ids_expanded * boundaries), [0])
 
-    province_owners = {}
     for province_id in range(1, num_provinces + 1):
-        colors, frequency = np.unique(provinces[province_ids == province_id], return_counts=True, axis=0)
+        boundaries = find_boundaries(province_id_map_expanded == province_id, mode='outer')
+        adjacencies[province_id] = np.setdiff1d(np.unique(province_id_map_expanded * boundaries), [0])
+
+    return adjacencies
+
+
+def get_province_owners(provinces, province_id_map, num_provinces):
+    province_owners = {}
+
+    for province_id in range(1, num_provinces + 1):
+        colors, frequency = np.unique(provinces[province_id_map == province_id], return_counts=True, axis=0)
         if len(colors) != 1:
             print(f"Province #{province_id} is not one solid color: {colors} with frequency: {frequency}.")
+
         top_color = colors[np.argmax(frequency)]
         assert tuple(top_color) in COLOR_PROVINCE_TYPE_MAP, \
             f"{tuple(top_color)} is not in the color to province type dictionary (province #{province_id})."
         province_owners[province_id] = COLOR_PROVINCE_TYPE_MAP[tuple(top_color)]
+
+    return province_owners
+
+
+if __name__ == '__main__':
+    read_map_data()
