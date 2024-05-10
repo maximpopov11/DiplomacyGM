4d7e8c4d8150af7b063737e0154624605c49ddd3
maximpopov11 <icecreamguy0@gmail.com>
2024-05-10 08:25:05 -0500

    Migrate User Constants to Config

---
+++ config.py
@@ -0,0 +1,19 @@
+PROVINCES_IMAGE = "assets/provinces.png"
+CENTERS_IMAGE = "assets/centers.png"
+UNITS_IMAGE = "assets/units.png"
+
+BORDER_COLOR = [0, 0, 0, 255]
+
+PROVINCE_COLOR_TYPE_MAP = {
+    (96, 96, 96): "impassable",
+    (197, 223, 234): "ocean",
+    (196, 143, 133): "brown",
+    (255, 56, 50): "red",
+    (255, 203, 91): "yellow",
+}
+
+UNIT_COLOR_TYPE_MAP = {
+    (221, 168, 158): "brown",
+    (255, 228, 116): "yellow",
+    (255, 81, 75): "red",
+}
--- main.py
+++ main.py
@@ -3,25 +3,8 @@ from PIL import Image
 from scipy import ndimage
 from skimage.segmentation import expand_labels, find_boundaries
 
-PROVINCES_IMAGE = "assets/provinces.png"
-CENTERS_IMAGE = "assets/centers.png"
-UNITS_IMAGE = "assets/units.png"
-
-BORDER_COLOR = [0, 0, 0, 255]
-
-PROVINCE_COLOR_TYPE_MAP = {
-    (96, 96, 96): "impassable",
-    (197, 223, 234): "ocean",
-    (196, 143, 133): "brown",
-    (255, 56, 50): "red",
-    (255, 203, 91): "yellow",
-}
-
-UNIT_COLOR_TYPE_MAP = {
-    (221, 168, 158): "brown",
-    (255, 228, 116): "yellow",
-    (255, 81, 75): "red",
-}
+from config import PROVINCES_IMAGE, CENTERS_IMAGE, UNITS_IMAGE, BORDER_COLOR, PROVINCE_COLOR_TYPE_MAP, \
+    UNIT_COLOR_TYPE_MAP
 
 
 def read_map_data():
