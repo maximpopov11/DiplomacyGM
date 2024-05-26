08ede6edcbdd503d52d99613abdc2a8840379d39
maximpopov11 <icecreamguy0@gmail.com>
2024-05-26 08:15:23 -0500

    Read basic SVG data

--- vector_input.py
+++ vector_input.py
@@ -1,1 +1,14 @@
-# SVG-input will live here
+from xml.dom import minidom
+
+if __name__ == '__main__':
+    svg = minidom.parse('assets/test.svg')
+    path_strings = [path.getAttribute('d') for path in svg.getElementsByTagName('path')]
+    svg.unlink()
+    # TODO: parse provinces
+        # TODO: populate territories
+        # TODO: populate province coordinates twice sorted x and y globally w/ province match
+        # TODO: record centers
+    # TODO: parse adjacencies
+        # TODO: iterate through coordinate windows and add to set all in range of a point's x and y
+    # TODO: parse players
+    # TODO: parse units
