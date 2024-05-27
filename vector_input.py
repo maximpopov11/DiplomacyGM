4dee74aac23debcabe9868865f3faad433c8082a
maximpopov11 <icecreamguy0@gmail.com>
2024-05-27 08:40:22 -0500

    Parse province coordinates

--- vector_input.py
+++ vector_input.py
@@ -1,9 +1,22 @@
-from xml.dom import minidom
+from lxml import etree
 
 if __name__ == '__main__':
-    svg = minidom.parse('assets/test.svg')
-    path_strings = [path.getAttribute('d') for path in svg.getElementsByTagName('path')]
-    svg.unlink()
+    map_data = etree.parse('assets/test.svg')
+
+    provinces = map_data.xpath('//*[@id="layer10"]')[0].getchildren()
+    coordinates = {}
+    for province in provinces:
+        province_id = province.get('id')
+        if not coordinates.get(province_id):
+            coordinates[province_id] = []
+
+        path = province.get('d').split()
+        for element in path:
+            split = element.split(',')
+            if len(split) != 2:
+                continue
+            coordinates.get(province_id).append((split[0], split[1]))
+
     # TODO: parse provinces
         # TODO: populate territories
         # TODO: populate province coordinates twice sorted x and y globally w/ province match
