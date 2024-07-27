1a1d81b57b0edbcdff010d42fc2c43e3a8e91846
maximpopov11 <icecreamguy0@gmail.com>
2024-07-26 19:11:08 -0500

    Implement bot framework

--- bot/bot.py
+++ bot/bot.py
@@ -1,96 +1,62 @@
+from typing import Callable
+
 import discord
 from discord.ext import commands
 
-import utils
-
+import command
 from _token import DISCORD_TOKEN
-from diplomacy.adjudicate import adjudicator
-from diplomacy.persistence import state
-from diplomacy.order import parse as parse_order
 
+# TODO: this and the main stuff should live in a class
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='.', intents=intents)
 
-# TODO: (FRAMEWORK) API only here, all actual implementations (that check perms, call into state, whatever) go into separate file
-# TODO: (FRAMEWORK) parse map to create state API
+# TODO: (FRAMEWORK) GM initial (or messed something up big time) API for parse map to create state
+# TODO: ensure all private commands (both player and GM) are illegal in non order/GM channels
 # TODO: implement & test all bot API commands
 # TODO: all commands should have an output/confirmation
 # TODO: GM manual correction API
 
-@bot.event
-async def on_ready():
-    print('Bot is ready!')
+
+commandFunctionType = Callable[[discord.ext.commands.Context], str]
 
 
-async def try_command(command, ctx):
+async def try_command(function: commandFunctionType, ctx: discord.ext.commands.Context):
     try:
-        await command(ctx)
+        response = function(ctx)
+        await ctx.channel.send(response)
     except Exception as e:
         await ctx.channel.send('Command errored with description: ' + str(e))
 
 
 @bot.command()
-async def ping(ctx):
-    await try_command(_ping, ctx)
-
-
-async def _ping(ctx):
-    await ctx.channel.send('Beep Boop')
+async def ping(ctx: discord.ext.commands.Context):
+    await try_command(command.ping, ctx)
 
 
 @bot.command()
-async def order(ctx):
-    await try_command(_order, ctx)
-
-
-async def _order(ctx):
-    response = parse_order(ctx.message.content)
-    await ctx.channel.send(response)
+async def order(ctx: discord.ext.commands.Context):
+    await try_command(command.order, ctx)
 
 
 @bot.command()
-async def view_orders(ctx):
-    await try_command(_view_orders, ctx)
-
-
-async def _view_orders(ctx):
-    player = utils.get_player(ctx.message.author)
-    response = board.get(player)
-    await ctx.channel.send(response)
+async def view_orders(ctx: discord.ext.commands.Context):
+    await try_command(command.view_orders, ctx)
 
 
 @bot.command()
-async def adjudicate(ctx):
-    await try_command(_adjudicate, ctx)
-
-
-async def _adjudicate(ctx):
-    # TODO: return moves & results
-    author = utils.get_player(ctx.message.author)
-    response = adjudicator.adjudicate(author)
-    await ctx.channel.send(response)
+async def adjudicate(ctx: discord.ext.commands.Context):
+    await try_command(command.adjudicate, ctx)
 
 
 @bot.command()
-async def rollback(ctx):
-    await try_command(_rollback, ctx)
-
-
-async def _rollback(ctx):
-    author = utils.get_player(ctx.message.author)
-    response = adjudicator.rollback(author)
-    await ctx.channel.send(response)
+async def rollback(ctx: discord.ext.commands.Context):
+    await try_command(command.rollback, ctx)
 
 
 @bot.command()
-async def scoreboard(ctx):
-    await try_command(_scoreboard, ctx)
-
-
-async def _scoreboard(ctx):
-    response = utils.get_scoreboard()
-    await ctx.channel.send(response)
+async def scoreboard(ctx: discord.ext.commands.Context):
+    await try_command(command.get_scoreboard, ctx)
 
 
 if __name__ == "__main__":
---
+++ bot/command.py
@@ -0,0 +1,47 @@
+from discord.ext import commands
+
+import diplomacy.order
+import diplomacy.persistence.state
+from diplomacy.adjudicate import adjudicator
+
+none = 'none'
+
+players = {
+    none,
+    'player 1',
+}
+
+
+# TODO: improve get role
+def get_player(author) -> str:
+    for role in author.roles:
+        if role in players:
+            return role
+    return none
+
+
+def ping(ctx: commands.Context) -> str:
+    return 'Beep Boop'
+
+
+def order(ctx: commands.Context) -> str:
+    return diplomacy.order.parse(ctx.message.content)
+
+
+def view_orders(ctx: commands.Context) -> str:
+    player = get_player(ctx.message.author)
+    return diplomacy.persistence.state.get(player)
+
+
+def adjudicate(ctx: commands.Context) -> str:
+    author = get_player(ctx.message.author)
+    return adjudicator.adjudicate(author)
+
+
+def rollback(ctx: commands.Context) -> str:
+    author = get_player(ctx.message.author)
+    return adjudicator.rollback(author)
+
+
+def get_scoreboard(ctx: commands.Context) -> str:
+    return 'pretend this is the scoreboard'
