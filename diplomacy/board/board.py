4e30715ad2fbfada96f90cfaa8dd6e7cd9de3d99
maximpopov11 <icecreamguy0@gmail.com>
2024-08-03 08:36:44 -0500

    Pass player centers

--- diplomacy/board/board.py
+++ diplomacy/board/board.py
@@ -1,7 +1,7 @@
-from typing import List, Set, Tuple, Mapping
+from typing import Mapping, Set
 
 from config import players
-from diplomacy.phase import spring_moves, Phase
+from diplomacy.phase import Phase, spring_moves
 from diplomacy.player import Player
 from diplomacy.province import Province
 from diplomacy.unit import Unit
@@ -13,21 +13,33 @@ class Board:
         self.provinces: Set[Province] = provinces
 
         self.players: Set[Player] = set()
-        units: Set[Unit] = get_units(provinces)
+        centers = get_centers(provinces)
+        units = get_units(provinces)
         for name in players:
-            self.players.add(Player(name, player_centers, units[name]))
+            self.players.add(Player(name, centers[name], units[name]))
 
         self.phase: Phase = spring_moves
 
 
-def get_units(provinces: List[Province]) -> Mapping[str, Set[Unit]]:
+def get_centers(provinces: Set[Province]) -> Mapping[str, Set[Province]]:
+    center_map = {}
+    for province in provinces:
+        if province.has_supply_center:
+            player = province.owner.name
+            if player not in center_map:
+                center_map[player] = set()
+            center_map[player].add(province)
+    return center_map
+
+
+def get_units(provinces: Set[Province]) -> Mapping[str, Set[Unit]]:
     unit_map = {}
     for province in provinces:
         unit = province.unit
         if unit is None:
             continue
 
-        player = unit.player
+        player = unit.player.name
         if player not in unit_map:
             unit_map[player] = set()
 
