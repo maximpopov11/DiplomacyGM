6b14d8b7ecfadd0320fa15d66aa97faedefa68d9
Jonathan Dingess <jonathan.dingess97@gmail.com>
2024-09-01 18:38:06 -0400

    Fixed the phantom transform data parsing, fixed province layer parsing

--- diplomacy/map_parser/vector/utils.py
+++ diplomacy/map_parser/vector/utils.py
@@ -1,11 +1,58 @@
+import re, sys
 from xml.etree.ElementTree import Element, ElementTree
 
 from diplomacy.persistence.player import Player
 from diplomacy.persistence.unit import UnitType
 
 
-def get_layer_data(svg_root: ElementTree, layer_id: str) -> list[Element]:
-    return svg_root.xpath(f'//*[@id="{layer_id}"]')[0].getchildren()
+# noinspection PyProtectedMember
+def get_svg_element_by_id(svg_root: ElementTree, element_id: str) -> Element:
+    return svg_root.xpath(f'//*[@id="{element_id}"]')[0]
+
+
+# NOTE: better way to do this DRY would be making a Transform class that can be applied to a coordinate and combining
+# these two functions into one. For now, I'm just doing it this way.
+def get_translation_for_element(element: Element) -> tuple[float, float]:
+    transform_string = element.get("transform", None)
+
+    if not transform_string:
+        return 0, 0
+
+    translation_match = re.search("^\\s*translate\\((.*),(.*)\\)\\s*", transform_string)
+    if not translation_match:
+        print(
+            f"Could not parse translate string {transform_string} on element with id {element.get("id", None)}",
+            file=sys.stderr,
+        )
+        return 0, 0
+
+    return float(translation_match.group(1)), float(translation_match.group(2))
+
+
+def get_matrix_transform_for_element(element: Element) -> tuple[float, float, float, float, float, float]:
+    transform_string = element.get("transform", None)
+
+    if not transform_string:
+        return 1, 0, 0, 1, 0, 0
+
+    translation_match = re.search("^\\s*matrix\\((.*),(.*),(.*),(.*),(.*),(.*)\\)\\s*", transform_string)
+    if not translation_match:
+        print(
+            f"Could not parse matrix transform string {transform_string} on element with id {element.get("id", None)}",
+            file=sys.stderr,
+        )
+        return 1, 0, 0, 1, 0, 0
+
+    # It kept whining at me about returning the wrong type when I did it in different ways
+    # I could suppress warning or find a way that doesn't throw warnings, but you know what? this works
+    return (
+        float(translation_match.group(1)),
+        float(translation_match.group(2)),
+        float(translation_match.group(3)),
+        float(translation_match.group(4)),
+        float(translation_match.group(5)),
+        float(translation_match.group(6)),
+    )
 
 
 def get_player(element: Element, color_to_player: dict[str, Player]) -> Player:
@@ -29,26 +76,19 @@ def _get_unit_type(unit_data: Element) -> UnitType:
 
 def _get_unit_coordinates_and_radius(
     unit_data: Element,
-    translation: tuple[float, float] = (0, 0),
 ) -> tuple[tuple[float, float], float]:
-    x = float(unit_data.get("{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}cx"))
-    y = float(unit_data.get("{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}cy"))
-    r = float(unit_data.get("{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}r2"))
+    path: Element = unit_data.find("{http://www.w3.org/2000/svg}path")
+    x = float(path.get("{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}cx"))
+    y = float(path.get("{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}cy"))
+    r = float(path.get("{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}r2"))
 
-    x += translation[0]
-    y += translation[1]
+    x_dx, y_dx, x_dy, y_dy, x_c, y_c = get_matrix_transform_for_element(path)
+    x = x_dx * x + y_dx * y + x_c
+    y = x_dy * x + y_dy * y + y_c
+    # Not sure what to do with r, but we don't use it anyway
+
     return (x, y), r
 
 
-def get_translation(element: Element) -> tuple[float, float]:
-    string = element.get("transform")
-    prefix = "translate("
-    if not string.startswith(prefix):
-        # TODO: (MAP) debug: translation is matrix not transform in select few cases
-        # raise RuntimeError(f"Translation transform expected, got: {string}")
-        return (0, 0)
-
-    string = string[len(prefix) : len(string) - 1]
-    nums: list[string] = string.split(",")
-
-    return float(nums[0]), float(nums[1])
+def add_tuples(*args: tuple[float, float]):
+    return sum([item[0] for item in args]), sum([item[1] for item in args])
