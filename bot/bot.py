9929993a48f6f039693c0e0d2cd6152ffdaf281b
maximpopov11 <icecreamguy0@gmail.com>
2024-09-09 20:27:01 +0400

    error logging

--- bot/bot.py
+++ bot/bot.py
@@ -45,7 +45,7 @@ async def _handle_command(
             f"[{ctx.guild.name}][#{ctx.channel.name}]({ctx.message.author.name}) "
             f"invoking '{getattr(function, '__name__', 'Unknown')}': {repr(e)}"
         )
-        logger.debug("", exc_info=e)
+        logger.error("", exc_info=e)
         await ctx.channel.send("Command errored: " + str(e))
 
 
