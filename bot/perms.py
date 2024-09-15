98d915a13badbbd759a9d5083aa809220dfa23f1
Kieran Bieniosek <kieran.bieniosek@gmail.com>
2024-09-15 13:53:02 -0700

    rework so testing works better; if you are both a gm and a player the player role will be prefered

--- bot/perms.py
+++ bot/perms.py
@@ -10,17 +10,15 @@ from diplomacy.persistence.player import Player
 def player(discription: str="run this command"):
     def player_check(function: Callable[[Player | None, commands.Context, Manager], tuple[str, str | None]]) -> Callable[[commands.Context, Manager], tuple[str, str | None]]:
         def f(ctx: commands.Context, manager: Manager) -> tuple[str, str | None]:
-            if is_gm(ctx.author):
-                if not is_gm_channel(ctx.channel):
-                    raise PermissionError(f"You cannot {discription} as a GM in a non-GM channel.")
-                player = None
-            else:
-                player = get_player_by_role(ctx.channel, manager, ctx.guild.id)
-                if player is None:
-                    raise PermissionError(f"You cannot {discription} because you are neither a GM nor a player.")
+            player = get_player_by_role(ctx.message.author, manager, ctx.guild.id)
+            if player:
                 if not is_player_channel(player.name, ctx.channel):
                     raise PermissionError(f"You cannot {discription} as a player outside of your orders channel.")
-
+            else:
+                if not is_gm(ctx.message.author):
+                    raise PermissionError(f"You cannot {discription} because you are neither a GM nor a player.")
+                if not is_gm_channel(ctx.channel):
+                    raise PermissionError(f"You cannot {discription} as a GM in a non-GM channel.")
             return function(player, ctx, manager)
         
         return f
