823cf355a68a39d0e75eaab302385a79a7f659fe
Kieran Bieniosek <kieran.bieniosek@gmail.com>
2024-09-21 10:02:42 -0700

    add sproc

---
+++ SQL/3-RenameProvinces.sql
@@ -0,0 +1,35 @@
+UPDATE units
+SET location = 'Sakalava'
+WHERE location = 'Sakalawa';
+
+UPDATE units
+SET location = 'Jassy'
+WHERE location = 'Jassey';
+
+UPDATE units
+SET location = 'Kathmandu'
+WHERE location = 'Kathmundu';
+
+UPDATE provinces
+SET province_name = 'Sakalava'
+WHERE province_name = 'Sakalawa';
+
+UPDATE provinces
+SET province_name = 'Jassy'
+WHERE province_name = 'Jassey';
+
+UPDATE provinces
+SET province_name = 'Kathmandu'
+WHERE province_name = 'Kathmundu';
+
+UPDATE builds
+SET location = 'Sakalava'
+WHERE location = 'Sakalawa';
+
+UPDATE builds
+SET location = 'Jassy'
+WHERE location = 'Jassey';
+
+UPDATE builds
+SET location = 'Kathmandu'
+WHERE location = 'Kathmundu';
