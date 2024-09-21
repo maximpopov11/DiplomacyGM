3fdcfa614185079589e36029b2ec0ee6a209f63c
Kieran Bieniosek <kieran.bieniosek@gmail.com>
2024-09-21 11:00:19 -0700

    fix orders for units

--- SQL/3-RenameProvinces.sql
+++ SQL/3-RenameProvinces.sql
@@ -10,6 +10,30 @@ UPDATE units
 SET location = 'Kathmandu'
 WHERE location = 'Kathmundu';
 
+UPDATE units
+SET order_destination = 'Sakalava'
+WHERE order_destination = 'Sakalawa';
+
+UPDATE units
+SET order_destination = 'Jassy'
+WHERE order_destination = 'Jassey';
+
+UPDATE units
+SET order_destination = 'Kathmandu'
+WHERE order_destination = 'Kathmundu';
+
+UPDATE units
+SET order_source = 'Sakalava'
+WHERE order_source = 'Sakalawa';
+
+UPDATE units
+SET order_source = 'Jassy'
+WHERE order_source = 'Jassey';
+
+UPDATE units
+SET order_source = 'Kathmandu'
+WHERE order_source = 'Kathmundu';
+
 UPDATE provinces
 SET province_name = 'Sakalava'
 WHERE province_name = 'Sakalawa';
