f9db8cfd2fa4d054ba707fe3251feb47043e9988
Kieran Bieniosek <kieran.bieniosek@gmail.com>
2024-10-05 15:56:57 -0700

    make holds capture

--- diplomacy/adjudicator/adjudicator.py
+++ diplomacy/adjudicator/adjudicator.py
@@ -395,6 +395,9 @@ class MovesAdjudicator(Adjudicator):
                 order.destination_province.unit = order.base_unit
                 if not order.destination_province.has_supply_center or self._board.phase.name.startswith("Fall"):
                     self._board.change_owner(order.destination_province, order.country)
+        if order.type == OrderType.HOLD and order.resolution == Resolution.SUCCEEDS:
+            if not order.destination_province.has_supply_center or self._board.phase.name.startswith("Fall"):
+                self._board.change_owner(order.destination_province, order.country)
         for unit in self._board.units:
             unit.order = None
 
