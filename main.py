6a4c946e054a74e608d2cdf884ece4d43a64ff32
maximpopov11 <icecreamguy0@gmail.com>
2024-10-06 10:17:31 -0500

    clean main

--- main.py
+++ main.py
@@ -2,18 +2,10 @@ import logging
 import os
 
 from bot import bot
-from test import functional_test
 
 log_level = logging.getLevelNamesMapping().get(os.getenv("log_level", "INFO"))
 if not log_level:
     log_level = logging.INFO
 logging.basicConfig(level=log_level)
-# from test import mapper_test
-
-# mapper_test.run()
 
 bot.run()
-
-# from test import db_test, functional_test
-
-# functional_test.run()
