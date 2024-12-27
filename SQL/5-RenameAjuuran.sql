c1ad3d8c373e8339431f7f38d92a86c3f0a4b9b1
Jonathan Dingess <jonathan.dingess97@gmail.com>
2024-12-27 17:47:44 -0600

    Rename Ajuuran -> Ajuuraan

---
+++ SQL/5-RenameAjuuran.sql
@@ -0,0 +1,15 @@
+UPDATE builds
+SET player = 'Ajuuraan'
+WHERE player = 'Ajuuran';
+
+UPDATE players
+SET player_name = 'Ajuuraan'
+WHERE player_name = 'Ajuuran';
+
+UPDATE provinces
+SET owner = 'Ajuuraan'
+WHERE owner = 'Ajuuran';
+
+UPDATE units
+SET owner = 'Ajuuraan'
+WHERE owner = 'Ajuuran';
--- diplomacy/map_parser/vector/config_player.py
+++ diplomacy/map_parser/vector/config_player.py
@@ -5,129 +5,29 @@ BLANK_CENTER = "ffffff"
 # name: [color, vscc, starting scs]
 
 player_data = {
-  "Abyssinia": [
-    "a88080",
-    32,
-    4
-  ],
-  "Ajuuran": [
-    "587ba7",
-    32,
-    4
-  ],
-  "Athapasca": [
-    "e95a94",
-    32,
-    4
-  ],
-  "Austria": [
-    "a17555",
-    32,
-    4
-  ],
-  "Aymara": [
-    "9a2f56",
-    32,
-    4
-  ],
-  "Ayutthaya": [
-    "9d84ae",
-    32,
-    4
-  ],
-  "England": [
-    "f94455",
-    56,
-    14
-  ],
-  "France": [
-    "66a2e3",
-    56,
-    14
-  ],
-  "Inuit": [
-    "afc5d8",
-    36,
-    5
-  ],
-  "Kongo": [
-    "9f52c4",
-    32,
-    4
-  ],
-  "Mali": [
-    "7a9025",
-    32,
-    4
-  ],
-  "Mapuche-Tehuelche": [
-    "81889d",
-    32,
-    4
-  ],
-  "Ming": [
-    "cadd7e",
-    36,
-    5
-  ],
-  "Mughal": [
-    "ab9f26",
-    36,
-    5
-  ],
-  "Netherlands": [
-    "fc9c23",
-    56,
-    14
-  ],
-  "Ottoman": [
-    "a03838",
-    48,
-    10
-  ],
-  "Poland-Lithuania": [
-    "e390b3",
-    42,
-    7
-  ],
-  "Portugal": [
-    "3c9553",
-    64,
-    16
-  ],
-  "Qing": [
-    "f36678",
-    36,
-    5
-  ],
-  "Russia": [
-    "556854",
-    48,
-    10
-  ],
-  "Safavid": [
-    "5a887f",
-    36,
-    5
-  ],
-  "Spain": [
-    "f0ce48",
-    64,
-    16
-  ],
-  "Sweden": [
-    "4e51ba",
-    32,
-    4
-  ],
-  "Tokugawa": [
-    "7cdbc3",
-    32,
-    4
-  ],
-  "Ute-Shoshone": [
-    "ca7a59",
-    36,
-    5
-  ]
+    "Abyssinia": ["a88080", 32, 4],
+    "Ajuuraan": ["587ba7", 32, 4],
+    "Athapasca": ["e95a94", 32, 4],
+    "Austria": ["a17555", 32, 4],
+    "Aymara": ["9a2f56", 32, 4],
+    "Ayutthaya": ["9d84ae", 32, 4],
+    "England": ["f94455", 56, 14],
+    "France": ["66a2e3", 56, 14],
+    "Inuit": ["afc5d8", 36, 5],
+    "Kongo": ["9f52c4", 32, 4],
+    "Mali": ["7a9025", 32, 4],
+    "Mapuche-Tehuelche": ["81889d", 32, 4],
+    "Ming": ["cadd7e", 36, 5],
+    "Mughal": ["ab9f26", 36, 5],
+    "Netherlands": ["fc9c23", 56, 14],
+    "Ottoman": ["a03838", 48, 10],
+    "Poland-Lithuania": ["e390b3", 42, 7],
+    "Portugal": ["3c9553", 64, 16],
+    "Qing": ["f36678", 36, 5],
+    "Russia": ["556854", 48, 10],
+    "Safavid": ["5a887f", 36, 5],
+    "Spain": ["f0ce48", 64, 16],
+    "Sweden": ["4e51ba", 32, 4],
+    "Tokugawa": ["7cdbc3", 32, 4],
+    "Ute-Shoshone": ["ca7a59", 36, 5],
 }
