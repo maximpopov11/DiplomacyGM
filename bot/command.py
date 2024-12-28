d928a18c8b1835183c7522da25b3f8a09c6f8834
Jonathan Dingess <jonathan.dingess97@gmail.com>
2024-12-27 18:11:48 -0600

    even yet more bugfix

--- bot/command.py
+++ bot/command.py
@@ -198,6 +198,6 @@ def province_info(ctx: commands.Context, manager: Manager) -> tuple[str, str | N
         f"Center: {province.has_supply_center}\n" + \
         f"Core: {province.core.name if province.core else 'None'}\n" + \
         f"Half-Core: {province.half_core.name if province.half_core else 'None'}\n" + \
-        f"Adjacent Provinces:\n- {"\n- ".join(sorted([adjacent.name for adjacent in province.adjacent]))}\n"
+        f"Adjacent Provinces:\n- {'\n- '.join(sorted([adjacent.name for adjacent in province.adjacent]))}\n"
     # fmt: on
     return out, None
