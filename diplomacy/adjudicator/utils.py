be749ea8693d936812cde48648c8828fd4916fa8
Royce <royce.yao.ry@gmail.com>
2025-03-18 09:26:25 -0700

    Don't return a png from .edit

--- diplomacy/adjudicator/utils.py
+++ diplomacy/adjudicator/utils.py
@@ -5,7 +5,6 @@ import os
 def svg_to_png(svg: bytes, file_name: str):
     db = usvg.FontDatabase.default()
     db.load_system_fonts()
-
     options = usvg.Options.default()
 
     tree = usvg.Tree.from_str(svg.decode(), options, db)
@@ -17,5 +16,4 @@ def svg_to_png(svg: bytes, file_name: str):
     data = render(tree, tr[0:6], bg_size=target_size)
 
     base = os.path.splitext(file_name)[0]
-
     return bytes(data), base + ".png"
