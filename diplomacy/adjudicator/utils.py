e27d9e35ce2bb4b7ad02ba97b379b836b80b3ebe
Nathan-Foster <91852769+Nathan-Foster@users.noreply.github.com>
2025-03-20 16:16:32 +0000

    Changing svg_to_png to use Inkscape and adding Docker (#118)
    * Changing svg_to_png to use Inkscape, this uses a janky setup to deal with inkscape warnings in stdout
    
    * Adding Docker
    
    * Removing resvg from requirements, removing rust install from dockerfile, and not requiring fonts for docker
    
    * removing unecissary affine requirement and import
    
    ---------
    
    Co-authored-by: Nathan Foster <nathan.foster@symmetrysolutions.co.uk>

--- diplomacy/adjudicator/utils.py
+++ diplomacy/adjudicator/utils.py
@@ -1,19 +1,22 @@
-from resvg import render, usvg
-import affine
+from subprocess import Popen, PIPE, STDOUT
 import os
 
 def svg_to_png(svg: bytes, file_name: str):
-    db = usvg.FontDatabase.default()
-    db.load_system_fonts()
-    options = usvg.Options.default()
 
-    tree = usvg.Tree.from_str(svg.decode(), options, db)
-    scale = 2
-    (w, h) = tree.int_size()
-    target_size = (w*scale, h*scale)
+    p = Popen(["inkscape", "--pipe", "--export-type=png"], stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    data = p.communicate(input=svg)[0]
 
-    tr = affine.Affine.scale(scale)
-    data = render(tree, tr[0:6], bg_size=target_size)
+    # Stupid inkscape error fix, not good but works
+    # Inkscape can throw warnings in stdout, this should remove those warnings, leaving us with a valid png
+
+    # This should indicate the start of the png, see https://www.w3.org/TR/2003/REC-PNG-20031110/#5PNG-file-signature
+    png_start = b'\x89PNG\r\n\x1a\n'
+
+    if data[:8] != png_start:
+        data = data[data.find(png_start):]
+
+        if data[:8] != png_start:
+            raise RuntimeError("Something went wrong with making the png.")
 
     base = os.path.splitext(file_name)[0]
     return bytes(data), base + ".png"
