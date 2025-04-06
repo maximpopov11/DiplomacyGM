8b1d8995b1583586bcef98657c555ca51eb1f010
Chloe Winter <git@eolhc.dev>
2025-04-06 05:22:30 +0100

    remove time footer from .order

--- bot/parse_order.py
+++ bot/parse_order.py
@@ -259,24 +259,6 @@ def parse_order(message: str, player_restriction: Player | None, board: Board) -
     for line in orderoutput:
         paginator.add_line(line)
 
-    match board.phase.name:
-        case "Spring Moves":
-            date = datetime.date(1642 + board.year, 3, 21)
-        case "Spring Retreats":
-            date = datetime.date(1642 + board.year, 6, 7)
-
-        case "Fall Moves":
-            date = datetime.date(1642 + board.year, 9, 22)
-
-        case "Fall Retreats":
-            date = datetime.date(1642 + board.year, 12, 7)
-        case "Winter Builds":
-            date = datetime.date(1642 + board.year, 12, 22)
-        case _:
-            date = datetime.date(1642 + board.year, 1, 1)
-
-    time = datetime.datetime.combine(date, datetime.datetime.now().time())
-
     output = paginator.pages
     if errors:
         output[-1] += "\n" + "\n".join(errors)
@@ -287,13 +269,11 @@ def parse_order(message: str, player_restriction: Player | None, board: Board) -
         return {
             "messages": output,
             "embed_colour": embed_colour,
-            "time": time,
         }
     else:
         return {
                 "title": "**Orders validated successfully.**",
                 "messages": output,
-                "time": time,
         }
 
 def parse_remove_order(message: str, player_restriction: Player | None, board: Board) -> dict[str, ...]:
