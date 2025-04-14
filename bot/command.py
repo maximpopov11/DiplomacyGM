7a9ca43a9fb85549d40cb7722d8e777750b1a743
Kieran Bieniosek <kieran.bieniosek@gmail.com>
2025-04-14 18:08:12 -0400

    whoops

--- bot/command.py
+++ bot/command.py
@@ -162,7 +162,7 @@ async def global_leaderboard(ctx: commands.Context, manager: Manager) -> None:
     if this_board is not None and this_board not in raw_boards[:9]:
         text += f"\n#{index} | {ctx.guild.name}"
     
-    await send_message_and_file(channel=ctx.channel
+    await send_message_and_file(channel=ctx.channel,
                                 title="Global Fishing Leaderboard",
                                 message=text)
 
