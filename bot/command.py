fdf59db1a5843a3d8b6eae462380c109a598c148
Kieran Bieniosek <kieran.bieniosek@gmail.com>
2025-06-05 17:47:33 -0700

    update .player_info

--- bot/command.py
+++ bot/command.py
@@ -784,10 +784,9 @@ async def player_info(ctx: commands.Context, manager: Manager) -> None:
     # f"Initial/Current/Victory SC Count [Score]: {player.iscc}/{len(player.centers)}/{player.vscc} [{player.score()}%]\n" + \
 
     # fmt: off
-    out = f"Player: {"TODO"}\n" + \
-        f"Color: #{player.render_color}\n" + \
+    out = f"Color: #{player.render_color}\n" + \
         f"Points: {player.points}\n" + \
-        f"Vassals: {"TODO"}\n" + \
+        f"Vassals: {', '.join(player.vassels)}\n" + \
         f"Liege: {player.liege if player.liege else "None"}\n" + \
         f"Units: {("\n- " + "\n- ".join([unit.location() for unit in player.units])) if len(player.units) > 0 else "None"}\n" + \
         f"Centers ({len(player.centers)}): {("\n- " + "\n- ".join([center.name for center in player.centers])) if len(player.centers) > 0 else "None"}\n"
