f2471ef3945edd1b1dddd1f56a46e37f2b50d8b7
ah1062 <ah1062@exeter.ac.uk>
2025-08-26 20:38:33 +0100

    initial commit
    # Ported .player_info from chaos
    - Encapsulated to an info() method attached to the Player() object.
    
    # Added .advertise
    - A standardised output for requesting substitiutes to ease process
    - NOTE: still needs manual deletion, will have to work on it
    
    # Created .substitute
    - Automatically handle exchange of roles in the Game server
    - Automatically handles logging to Reputation-Tracker
    
    - Fixed error of .help breaking for local bot instances from assert_mod_only RuntimeError
    - Added new config constants for relevant channels
    
    ### Formatter woes :(

--- diplomacy/persistence/player.py
+++ diplomacy/persistence/player.py
@@ -10,15 +10,19 @@ if TYPE_CHECKING:
     from diplomacy.persistence import province
     from diplomacy.persistence import unit
 
+
 class VassalType(Enum):
-    '''Needed due to ambiguity, especially after fall moves but before fall retreats'''
-    VASSAL = 'vassal'
-    DUAL = 'dual'
+    """Needed due to ambiguity, especially after fall moves but before fall retreats"""
+
+    VASSAL = "vassal"
+    DUAL = "dual"
+
 
 class PlayerClass(Enum):
     DUCHY = 0
     KINGDOM = 1
     EMPIRE = 2
+
 
 class Player:
     def __init__(
@@ -62,6 +66,29 @@ class Player:
 
     def __str__(self):
         return self.name
+
+    def info(self, variant: str = "standard") -> str:
+        bullet = "\n- "
+
+        if variant == "chaos":
+            out = (
+                f"Color: #{self.render_color}\n"
+                + f"Points: {self.points}\n"
+                + f"Vassals: {', '.join(map(str,self.vassals))}\n"
+                + f"Liege: {self.liege if self.liege else 'None'}\n"
+                + f"Units: {(bullet + bullet.join([unit.location().name for unit in self.units])) if len(self.units) > 0 else 'None'}\n"
+                + f"Centers ({len(self.centers)}): {(bullet + bullet.join([center.name for center in self.centers])) if len(self.centers) > 0 else 'None'}\n"
+            )
+            return out
+
+        out = (
+            ""
+            + f"Color: {(bullet + bullet.join([k + ': ' + v for k, v in self.color_dict.items()]) if self.color_dict is not None else self.render_color)}\n"
+            + f"Score: [{len(self.centers)}/{self.vscc}] {round(self.score() * 100, 2)}%\n"
+            + f"Centers: {(bullet + bullet.join([center.name for center in self.centers])) if len(self.centers) > 0 else 'None'}\n"
+            + f"Units: {(bullet + bullet.join([unit.location().name for unit in self.units])) if len(self.units) > 0 else 'None'}\n"
+        )
+        return out
 
     def score(self):
         if len(self.centers) > self.iscc:
